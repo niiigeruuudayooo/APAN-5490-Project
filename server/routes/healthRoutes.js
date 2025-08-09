17491084a371524072e6fbbeee58f5ad5f28344e
JonasHe1101 <xh2671@columbia.edu>
2025-08-09 12:39:09 -0400

    delete healthRoutes.js

--- server/routes/healthRoutes.js
+++ server/routes/healthRoutes.js
@@ -1,7 +1,1 @@
-// server/routes/healthRoutes.js
-const express = require('express');
-const router = express.Router();
 
-router.get('/healthz', (req, res) => res.json({ ok: true }));
-
-module.exports = router;
