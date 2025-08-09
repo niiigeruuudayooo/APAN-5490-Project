dfdb34287f892bd994379b7106e4270a5dcdfc55
JonasHe1101 <xh2671@columbia.edu>
2025-08-09 01:39:58 -0400

    Create healthRoutes.js

---
+++ server/routes/healthRoutes.js
@@ -0,0 +1,7 @@
+// server/routes/healthRoutes.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/healthz', (req, res) => res.json({ ok: true }));
+
+module.exports = router;
