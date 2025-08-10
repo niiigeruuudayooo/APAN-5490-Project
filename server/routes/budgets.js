f6b6c0fe183670657a1a1c1050992927592b7a73
zl3514 <zl3514@columbia.edu>
2025-08-10 01:13:48 -0400

    Update budgets.js

--- server/routes/budgets.js
+++ server/routes/budgets.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const auth = require('../middlewares/auth');
+const auth = require('../middleware/auth');
 const ctrl = require('../controllers/budgetsCtrl');
 
 router.get('/', auth, ctrl.getByMonth);
